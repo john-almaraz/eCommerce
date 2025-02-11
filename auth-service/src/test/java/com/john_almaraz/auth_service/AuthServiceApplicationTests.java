946c9f51c097041e0d140ed059bcd888b58ab4d9
john-almaraz <125034147+john-almaraz@users.noreply.github.com>
2025-02-11 15:56:34 +0100

    initial commit: each microservice with hexagonal architecture

---
+++ auth-service/src/test/java/com/john_almaraz/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john_almaraz.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/com/john_almaraz/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john_almaraz.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
